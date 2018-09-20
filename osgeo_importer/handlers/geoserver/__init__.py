3eecab8551ac589788752dac8c07e05c5d5bd10f
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2018-09-20 12:09:11 +0200

    Celery 4 Support
    Co-authored-by: Ahmed Nour <ahmednoreldeen@gmail.com>
    Co-authored-by: Hisham Karam <24391550+hishamkaram@users.noreply.github.com>

--- osgeo_importer/handlers/geoserver/__init__.py
+++ osgeo_importer/handlers/geoserver/__init__.py
@@ -10,6 +10,7 @@ from geoserver.catalog import FailedRequestError, ConflictingDataError
 from geonode.geoserver.helpers import gs_catalog
 from geonode.upload.utils import make_geogig_rest_payload, init_geogig_repo
 from geonode.geoserver.helpers import get_sld_for, _style_contexts, _style_templates, save_style
+from geonode.people.models import Profile
 from geoserver.catalog import ConflictingDataError
 from geoserver.support import DimensionInfo
 from osgeo_importer.utils import increment_filename, database_schema_name
@@ -261,6 +262,9 @@ class GeoserverPublishHandler(GeoserverHandlerMixin):
             return
 
         request_user = kwargs.get('request_user', None)
+
+        if request_user:
+            request_user = Profile.objects.get(username=request_user)
         store = self.get_or_create_datastore(layer_config, request_user)
 
         store_type = getattr(store, 'type', None) or ''
